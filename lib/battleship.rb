0c5e26b26668c2faa3114e52ae9b9f398b282648
Franklin Webber <franklin.webber@gmail.com>
2012-05-01 16:25:45 -0700

    Updated to add more state and output

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -1,20 +1,48 @@
 class Battleship
-  
+
   attr_accessor :players
   attr_accessor :ships
   attr_accessor :ship_positions
   
+  attr_reader :game_is_over
+  attr_reader :game_has_started
+  
+  def game_is_over?
+    @game_is_over
+  end
+  
+  def game_has_started?
+    @game_has_started
+  end
+  
   def initialize
+    @game_is_over = false
     @players = []
     @ship_positions = {}
   end
   
   def add_player(name)
+    
+    if game_has_started?
+      puts "Sorry #{name}, you can't play. The game has already started"
+      return
+    end
+    
     @players << name
     @ship_positions[name] = {}
   end
   
   def place_ship(player_name,ship_name,direction,x_position,y_position)
+    
+    if game_is_over?
+      puts "GAME IS OVER! Start a new game!"
+      return
+    end
+    
+    if game_has_started?
+      puts "Sorry #{player_name}, you can't place a ship. The game has already started"
+      return
+    end
     
     horizontal = (direction == :horizontal) ? 1 : 0
     vertical = (direction == :vertical) ? 1 : 0
@@ -43,14 +71,21 @@ class Battleship
   
   def shoot(at_player_with_name,x_position,y_position)
     
-    puts "CHEAT: #{@ship_positions[at_player_with_name].inspect}"
+    if game_is_over?
+      puts "GAME IS OVER! Start a new game!"
+      return
+    end
+    
+    @game_has_started = true
+    
+    # puts "CHEAT: #{@ship_positions[at_player_with_name].inspect}"
     
     sunk_ship = @ship_positions[at_player_with_name].find do |ship_name,ship_positions|
       
       hit_at_position = ship_positions.find do |position|
         
         if (position.first == x_position and position.last == y_position)
-          puts "HIT!"
+          puts "#{at_player_with_name}'s #{ship_name} has been HIT!"
           true
         else
           puts "MISS!"
@@ -74,12 +109,13 @@ class Battleship
     end
     
     if sunk_ship
-      puts "YOU SUNK MY #{sunk_ship.first}!"
+      puts "YOU HAVE SUNK #{at_player_with_name}'s #{sunk_ship.first}!"
       @ship_positions[at_player_with_name].delete sunk_ship.first
     end
     
     if @ship_positions[at_player_with_name].empty?
       puts "PLAYER #{at_player_with_name} has been defeated!"
+      @game_is_over = true
     end
     
   end
@@ -93,5 +129,8 @@ b.place_ship 'frank', :tugboat, :horizontal, 1, 1
 b.place_ship 'ivan', :carrier, :vertical, 1, 1
 
 b.shoot 'frank', 1, 1
+b.add_player 'brandon'
+b.place_ship 'brandon', :destroyer, :horizonal, 1, 3
 b.shoot 'frank', 1, 1
+b.shoot 'frank', 2, 1
 b.shoot 'frank', 2, 1
